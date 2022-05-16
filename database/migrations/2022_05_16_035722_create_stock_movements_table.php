a80ff38e35d4577d37677bfcbf43d8c9717a7e29
steven1303 <steven.grafensen@gmail.com>
2022-05-16 13:40:42 +0800

    Migration stock Movement

--- database/migrations/2022_05_16_035722_create_stock_movements_table.php
+++ database/migrations/2022_05_16_035722_create_stock_movements_table.php
@@ -15,6 +15,20 @@ return new class extends Migration
     {
         Schema::create('stock_movements', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('id_stock_master');
+            $table->bigInteger('id_branch');
+            $table->dateTime('move_date');
+            $table->string('type');
+            $table->string('doc_no');
+            $table->decimal('order_qty', 15, 3)->default(0);
+            $table->decimal('sell_qty', 15, 3)->default(0);
+            $table->decimal('in_qty', 15, 3)->default(0);
+            $table->decimal('out_qty', 15, 3)->default(0);
+            $table->decimal('harga_modal', 20, 3)->default(0);
+            $table->decimal('harga_jual', 20, 3)->default(0);
+            $table->string('user');
+            $table->integer('status')->default(0);
+            $table->string('ket')->nullable();
             $table->timestamps();
         });
     }
