f6017a008e460ac3104e5f3223bfa02ea6db12f9
Steven Grafensen <steven.grafensen@gmail.com>
2022-10-08 22:21:48 +0800

    Config global Setting

--- config/mito.php
+++ config/mito.php
@@ -2,7 +2,13 @@
 
 return [
     "tax" => [
-        "name" => "10 %",
+        "name" => "11 %",
         "decimal" => 0.11,
+    ],
+    "master" => 1,
+    "format" => [
+        "date" => "",
+        "time" => "",
+        "money" => ""
     ]
 ];
